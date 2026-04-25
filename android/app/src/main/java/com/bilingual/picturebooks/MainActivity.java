593cf7fb1945b55a494e65ea1664891fc04433a8
Deploy <nonomm996@gmail.com>
2026-04-25 20:04:44 +0800

    Add network security config for WebView remote mode + allow cleartext

--- android/app/src/main/java/com/bilingual/picturebooks/MainActivity.java
+++ android/app/src/main/java/com/bilingual/picturebooks/MainActivity.java
@@ -3,6 +3,4 @@ package com.bilingual.picturebooks;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
-    // TTS 插件通过 @CapacitorPlugin 注解自动注册
-    // 无需手动注册
 }
